44a732066ba1612b6c278dd24b5b82d7eb8fc87f
Jason Rodriguez <86337121+jrodriq3@users.noreply.github.com>
2024-10-01 15:16:21 -0400

    Updated EditCustomerServlet
    Removed default starting servlet text wrote code to get the customerID from the request then create new customer object and use the selectDB method and store the new customer object in the session then send that data through the request to edit-customer.jsp

--- ASPOnlinePetMarket/src/main/java/EditCustomerServlet.java
+++ ASPOnlinePetMarket/src/main/java/EditCustomerServlet.java
@@ -3,38 +3,22 @@ import java.io.PrintWriter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
-
-/**
- *
- * @author jason
- */
+import Business.Customer;
 @WebServlet(urlPatterns = {"/EditCustomerServlet"})
 public class EditCustomerServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet EditCustomerServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet EditCustomerServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        String customerID = request.getParameter("customerID");
+        Customer customer = new Customer();
+        customer.setCustomerID(Integer.parseInt(customerID));
+        customer.selectDB();
+        request.setAttribute("customer", customer);
+        request.getRequestDispatcher("edit-customer.jsp").forward(request, response);
+        
+        
+        
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
